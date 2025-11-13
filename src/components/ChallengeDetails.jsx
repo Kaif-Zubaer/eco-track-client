89d0c670b65842c446612c01ae746edb7498bbaa
Kaif-Zubaer <kaifhossainzubaer@gmail.com>
2025-11-13 20:18:03 +0600

    challenge details added

--- src/components/ChallengeDetails.jsx
+++ src/components/ChallengeDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { Link, useLoaderData } from 'react-router';
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { FaUser } from "react-icons/fa";
 
@@ -20,39 +20,40 @@ const ChallengeDetails = () => {
     } = challengeDetails;
 
     return (
-        <div>
-            <h3 className='flex items-center mx-3 my-2 text-xl gap-4 font-bold text-accent'><IoMdArrowRoundBack /> Back to Challenges</h3>
-            <div className='mt-3 mb-10'>
-                <div>
-                    <img src={imageUrl} alt="" />
-                    <p className='hidden text-lg mt-5 px-6 text-justify'>{description}</p>
+        <div className="container mx-auto p-4 md:p-8 mb-10">
+            <Link to="/challenges" className='flex items-center gap-2 text-accent font-semibold mb-4 hover:underline'>
+                <IoMdArrowRoundBack className="text-2xl" /> Back to Challenges
+            </Link>
+            <div className="flex flex-col md:w-4/5 xl:w-full mx-auto xl:flex-row bg-white shadow-lg rounded-lg overflow-hidden">
+                <div className="">
+                    <img src={imageUrl} alt={title} className="w-full h-full object-cover" />
                 </div>
-                <div className='px-6'>
-                    <h1 className='text-2xl font-bold text-primary pt-5 pb-2 border-b-2'>{title}</h1>
-                    <h3 className='text-xl font-bold text-accent py-2 border-b-2 border-primary'>{target}</h3>
-                    <div className='flex items-center py-3 gap-3 border-b-2 border-primary'>
-                        <p className='text-lg text-accent font-bold'>Category:</p>
-                        <p className='py-1 px-5 bg-primary text-white font-semibold rounded-4xl'>{category}</p>
+                <div className="xl:w-1/2 p-6 flex flex-col justify-between">
+                    <div>
+                        <h1 className="mb-3 text-3xl md:text-4xl font-bold text-accent">{title}</h1>
+                        <p className="text-lg font-semibold text-primary mb-2">{target}</p>
+                        <div className="flex items-center flex-wrap gap-4 my-3">
+                            <span className="px-4 py-1 border-2 border-accent bg-accent text-white rounded-full text-sm font-semibold">{category}</span>
+                            <span className="px-4 py-1 border-2 border-accent bg-white text-accent rounded-full text-sm font-semibold">{impactMetric}</span>
+                        </div>
+                        <div className="flex justify-between items-center mb-3 text-accent font-medium">
+                            <p className="font-bold text-accent">Duration: <span className="text-primary font-bold">{duration} days</span></p>
+                            <p className="flex items-center gap-1"><FaUser className="text-primary" /> {participants} Participants</p>
+                        </div>
 
+                        <div className="text-gray-500 font-semibold mb-3">
+                            <p className="mb-1"><span className="font-bold text-accent">Created By:</span> {createdBy}</p>
+                            <p className="mb-1"><span className="font-bold text-accent">Start Date:</span> {new Date(startDate).toLocaleDateString()}</p>
+                            <p className="mb-1"><span className="font-bold text-accent">End Date:</span> {new Date(endDate).toLocaleDateString()}</p>
+                        </div>
+                        <p className="font-medium text-justify">{description}</p>
                     </div>
-                    <div className='flex justify-between pr-8 py-3 text-lg font-bold text-accent border-b-2 border-primary'>
-                        <p>Duration: <span className='text-primary'>{duration} Days</span></p>
-                        <p className='flex justify-center items-center gap-4'><FaUser className='text-primary' /> Participants: <span className='text-primary'>{participants}</span></p>
+                    <div className="mt-6">
+                        <button className="w-fit bg-accent text-white font-bold py-2 px-6 rounded-sm shadow-lg hover:opacity-85 cursor-pointer transition duration-300">
+                            Join Challenge
+                        </button>
                     </div>
-                    <h3>{impactMetric}</h3>
-                    <p>{createdBy}</p>
-                    <div>
-                        <p>{startDate}</p>
-                        <p>{endDate}</p>
-                    </div>
-                    <div>
-                        <p className='text-lg mt-5 text-justify'>{description}</p>
-                    </div>
-                    <button>Join</button>
                 </div>
-            </div>
-            <div>
-                <h2>Participent:</h2>
             </div>
         </div>
     );
